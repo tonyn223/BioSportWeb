e71c87415eecb42d226fdff64947fb42559e850b
tonyn223 <tnvrgs2@gmail.com>
2026-06-24 23:17:11 -0600

    CRUD de Planes Completado

--- Controllers/PlanesController.cs
+++ Controllers/PlanesController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using BioSportWeb.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace BioSportWeb.Controllers
 {
@@ -13,11 +15,117 @@ namespace BioSportWeb.Controllers
             _context = context;
         }
 
+        // GET: Planes
         public async Task<IActionResult> Index()
         {
-            // Esto intenta traer los datos de tu tabla 'Planes' en SQL
             var planes = await _context.Planes.ToListAsync();
             return View(planes);
+        }
+
+        // GET: Planes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Planes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nombre,Precio,DuracionDias")] BioSportWeb.Models.Planes plan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(plan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        // GET: Planes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Planes.FindAsync(id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+            return View(plan);
+        }
+
+        // POST: Planes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPlan,Nombre,Precio,DuracionDias")] BioSportWeb.Models.Planes plan)
+        {
+            if (id != plan.IdPlan)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(plan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlanExists(plan.IdPlan))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        // --- NUEVO: GET: Planes/Delete/5 (Muestra la pantalla de confirmación) ---
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Planes.FirstOrDefaultAsync(m => m.IdPlan == id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return View(plan);
+        }
+
+        // --- NUEVO: POST: Planes/Delete/5 (Ejecuta el borrado real en la BD) ---
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var plan = await _context.Planes.FindAsync(id);
+            if (plan != null)
+            {
+                _context.Planes.Remove(plan);
+                await _context.SaveChangesAsync();
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlanExists(int id)
+        {
+            return _context.Planes.Any(e => e.IdPlan == id);
         }
     }
 }
